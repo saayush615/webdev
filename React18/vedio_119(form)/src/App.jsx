2556febef57f3bd7c34dd1cd1315195717f90b2b
Aayush singh <saayush615@gmail.com>
2025-06-27 23:25:03 +0530

    Learned react-hook-form completely

--- React18/vedio_119(form)/src/App.jsx
+++ React18/vedio_119(form)/src/App.jsx
@@ -9,21 +9,56 @@ function App() {
     register,
     handleSubmit,
     watch,
-    formState: { errors },
+    setError, // custom error
+    formState: { errors, isSubmitting },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data)
+  const delay = (d) => { 
+    return new Promise((resolve,reject) => { 
+      setTimeout(() => {
+        resolve()
+      }, d * 1000);
+     })
+   }
+
+  const onSubmit = async (data) => {
+    // await delay(3) // simulating network delay
+
+    // Post request to backend
+    let r = await fetch("http://localhost:3000/", {method: "POST", headers: {
+      "Content-Type": "application/json", 
+    }, body: JSON.stringify(data)})
+    let res = await r.text()
+
+    console.log(data,res)
+
+    // if (data.username !== "Aayush") {
+    //   setError("myform", {message: "Credential donot match"}) // jis v key pe error dena chahte ho (myform in this case)
+    // }
+    // if(data.username === "Raj") {
+    //   setError("blocked", {message: "You have been blocked by admin"}) // jis v key pe error dena chahte ho (myform in this case)
+    // }
+  }
 
   return (
     <>
+    {isSubmitting && 'Loading...'}
       <div className="container">
-        <form action="onSubmit={handleSubmit(onSubmit)}">
-          <input placeholder='username' {...register("username", { required: true, minLength: 3, maxLength: 8 })} type="text" />
-          {errors.username && <span>Username is required</span>}
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
+
+          <input placeholder='username' {...register("username", { required: {value: true, message: 'This field is required'}, minLength: {value: 3, message: 'Min length is 3'}, maxLength: {value: 8, message: 'Max length is 8'} })} type="text" />
+          {errors.username && <div>{errors.username.message}</div>}
+
           <br />
-          <input placeholder='password' {...register("password")} type="password" />
+
+          <input placeholder='password' {...register("password", {required: {value: true, message:'This is required field'}, minLength: {value: 4, message: 'There must 4 digit in password'}})} type="password" />
+          {errors.password && <div>{errors.password.message}</div>}
+
           <br />
-          <input type="submit" value="Submit" />
+
+          <input disabled={isSubmitting} type="submit" value="Submit" />
+          {errors.myform && <div>{errors.myform.message}</div>}
+          {errors.blocked && <div>{errors.blocked.message}</div>}
         </form>
       </div>
     </>
